79c489c3d14a7c0e3ff3d725390cf969cf7978dd
sleekLancelot <sleekzy7@gmail.com>
2023-03-11 19:13:02 +0100

    correct the progress bar color-scheme

--- src/Components/Table/ProgressBar.tsx
+++ src/Components/Table/ProgressBar.tsx
@@ -18,7 +18,7 @@ const ProgressBar = ({
     },[sec, funcToCall])
     
   return (
-    <Progress colorScheme={'green'} value={progress} height="2px" w="90%" h={15} borderRadius={16} />
+    <Progress colorScheme={'green'} color={'#3BC171'} value={progress} height="2px" w="90%" h={15} backgroundColor={'#B2B2B2'} borderRadius={8} />
   )
 }
 
